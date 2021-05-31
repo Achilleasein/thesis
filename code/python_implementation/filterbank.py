368f87b92a4f37dd6b6c876d63c8ae4158947a62
achilleasein <achilleasein@gmail.com>
2021-05-31 19:44:25 +0300

    backup

--- code/python_implementation/filterbank.py
+++ code/python_implementation/filterbank.py
@@ -1,6 +1,33 @@
+from numpy.lib.function_base import blackman
 import scipy.fftpack as fftfuncs
+from scipy.linalg import dft
+import numpy
+import math
 
 def filterbank(sig, bandlimits=[0,200,400,800,1600,3200], maxfreq=4096):
     
-    fftfuncs.fft()
-    return 0 
+    transformed_sig = fftfuncs.fft(sig)
+    
+    n = len(sig)
+    nbands = len(bandlimits)
+    iters = nbands - 1
+    bl = [0]*iters
+    br = [0]*iters
+
+    for i in range(1,iters):
+        bl[i] = math.floor(bandlimits[i]/maxfreq*n/2)+1
+        br[i] = math.floor(n/2)
+
+    bl[nbands-2] = math.floor(bandlimits[i]/maxfreq*n/2)+1
+    br[nbands-2] = math.floor(n/2)
+    
+    output = numpy.zeros((n,nbands))
+
+    for i in range(1,nbands):
+        output[bl[i]:br[i]:i] = transformed_sig[bl[i]:br[i]]
+        output[n+1-br[i]:n+1-bl[i]:i] = transformed_sig[n+1-br[i]:n+1-bl[i]]
+        
+
+    output[0,0] = 0
+
+    return output
--- code/python_implementation/main.py
+++ code/python_implementation/main.py
@@ -2,4 +2,5 @@ import filterbank
 
 if __name__ == "__main__":
     print('Rythm detection begins.')
-    print(filterbank.filterbank())
+    res = print(filterbank.filterbank([1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9]))
+    print(res)
